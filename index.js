6121e07ddd0fe14f3f128951f0b5021a5f1079d0
nirmitchattoo <2020bteit00001@walchandsangli.ac.in>
2022-09-30 16:38:54 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+const data = require('data.json');
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    return res.json(
+        {
+            message: "Horror Movies API"
+        }
+    )
+});
+
+//any random
+app.get('/random', (req,res)=>{
+    const numberOfElements = data.length;
+    const randomNumber = parseInt(Math.random()*numberOfElements);
+    return res.json(data[randomNumber])
+});
+
+//list
+
+
+// app.listen(5000, ()=>{
+//     console.log("Server has stated");
+// });
+
+module.exports = app;
+
